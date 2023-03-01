316428fc82a0e4028dab5231c075d944bcbac29d
zhangpan <zhangpan@uu898.com>
2023-03-01 20:53:28 +0800

    修改版本号为0.3.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/zhangpan-soft/py-commons'
 EMAIL = 'zhangpan_soft@163.com'
 AUTHOR = 'zhangpan_soft'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.2'
+VERSION = '0.3.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
